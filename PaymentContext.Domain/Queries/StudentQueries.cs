68b38cd286b2cd1cc89177b7cfd9f1bcbc92f16d
Diego Aquino <diegol.aquino@gmail.com>
2020-06-11 16:37:35 -0300

    episódio testando queries

--- PaymentContext.Domain/Queries/StudentQueries.cs
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using PaymentContext.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace PaymentContext.Domain.Queries
 {
-    public class StudentQueries
+    public static class StudentQueries
     {
+        public static Expression<Func<Student, bool>> GetStudentInfo(string document)
+        {
+            return x => x.Document.Number == document;
+        }
     }
 }
---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private readonly IList<Student> _students;
+
+        public StudentQueriesTests()
+        {
+            _students = new List<Student>();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                _students.Add(
+                    new Student(
+                        new Name("Aluno", i.ToString()), 
+                        new Document("12960405775", EDocumentType.CPF), 
+                        new Email("diegol.aquino@gmail.com")
+                    )
+                 );
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("12345678911");
+
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreNotEqual(null, studn);
+        }
+    }
+}
